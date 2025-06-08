e904393d8b131b045ef6ce40e277cc48e4981f06
Dilrukshi Siriwardena <dilrukshimelisa98@gmail.com>
2025-06-09 01:27:38 +0530

    Initial commit

--- src/main/java/com/example/lostandfound/LostandfoundApplication.java
+++ src/main/java/com/example/lostandfound/LostandfoundApplication.java
@@ -9,6 +9,7 @@ public class LostandfoundApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LostandfoundApplication.class, args);
 		System.out.println("abc");
+		System.out.println("abc");
 
 	}
 
